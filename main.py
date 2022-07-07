223aa232bdb06651de2cbd0b5bcdb346f4af981d
ChinomnsoC <chinomnsoc@gmail.com>
2022-07-07 17:35:36 +0100

    built a data pipeline with three successive tasks

--- main.py
+++ main.py
@@ -34,8 +34,24 @@ class GenerateReport(Task):
                 print(month + ',' + str(report[month]), file=out)
 
 
+class SummarizeReport(Task):
+    def requires(self):
+        return GenerateReport()
+
+    def output(self):
+        return LocalTarget('summary.txt')
+
+    def run(self):
+        total = 0.0
+        for line in self.input().open():
+            month, amount = line.split(',')
+            total += float(amount)
+        with self.output().open('w') as f:
+            f.write(str(total))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    luigi.run(['GenerateReport', '--local-scheduler'])
+    luigi.run(['SummarizeReport'])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
